701b259b5de5168c8d0eb7372a38e65d9b427674
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2022-11-23 12:33:15 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,9 +11,9 @@ var currentLife = 0;               //C42// SA
 
 function preload() {
   backgroundImage = loadImage("./assets/background.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/track.jpg");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/track.jpg");
   fuelImage = loadImage("./assets/fuel.png");
   powerCoinImage = loadImage("./assets/goldCoin.png");
   lifeImage = loadImage("./assets/life.png");
